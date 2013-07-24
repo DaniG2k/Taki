630a42f78780282d4d5790d0dd70661f99ef12a5
DaniG2k <byakugan.87@gmail.com>
2013-07-24 16:20:45 +0100

    Edited user.rb
    Removed references to tutor and student

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  has_one :tutor, dependent: :destroy
-  has_one :student, dependent: :destroy
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
