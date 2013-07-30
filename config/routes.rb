a42bb4707cf249b0103d0d496cc17d2da778da65
DaniG2k <byakugan.87@gmail.com>
2013-07-30 12:29:40 +0100

    Modified home#index to root in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Taki::Application.routes.draw do
+  # You can have the root of your site routed with "root"
+  root 'home#index'
+  
   get "users/show"
   devise_for :users
-  get "home/index"
   get "home/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
