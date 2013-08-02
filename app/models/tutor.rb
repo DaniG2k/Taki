2809a61831b4d9898196eda38ddb0f5ebd7622a5
DaniG2k <byakugan.87@gmail.com>
2013-08-02 14:42:30 +0100

    Commented numericality validation for rate

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,4 +1,8 @@
 class Tutor < ActiveRecord::Base
   belongs_to :user
-  validates :rate, numericality: {greater_than: 0, less_than: 1000}
+  # The value of is_tutor needs to be found dynamically
+  # Otherwise, the form will never validate because the value of is_tutor is true
+  # since the first time that the form is edited, whcih is when the Tutor object is
+  # created.
+  #validates :rate, numericality: {greater_than: 0, less_than: 1000}, if: -> {user.is_tutor}
 end
