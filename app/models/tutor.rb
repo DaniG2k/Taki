f1f2b4129d9077f94fe6166e50af9797d97ef44d
DaniG2k <byakugan.87@gmail.com>
2013-08-13 21:59:07 +0100

    Added maxlength validator for description

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,8 +1,9 @@
 class Tutor < ActiveRecord::Base
   belongs_to :user
   validates_numericality_of :rate, greater_than: 0
-  
-  def to_param
-    "#{user.first_name}".parameterize
-  end
+  validates_length_of :description, maximum: 1000, allow_blank: true
+  # This can be used for SEO friendliness
+  #def to_param
+  #  "#{user.first_name}".parameterize
+  #end
 end
