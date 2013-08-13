e4d1b7a773ba1148a82b671193dd58e16049f969
DaniG2k <byakugan.87@gmail.com>
2013-08-13 16:54:17 +0100

    Made locale routes work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,14 +13,9 @@ class ApplicationController < ActionController::Base
     end
     
     def set_locale
-      I18n.locale = extract_locale_from_tld || I18n.default_locale
+      I18n.locale = params[:locale] || I18n.default_locale
     end
-    
-    def extract_locale_from_tld
-      parsed_locale = request.host.split('.').last
-      I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
-    end
-    
+
     def default_url_options(options={})
       logger.debug "default_url_options is passed options: #{options.inspect}\n"
       {locale: I18n.locale} 
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,6 @@ class User < ActiveRecord::Base
   validate :user_age, on: :update
   
   private
-    def self.lang_list
-      [%w(English en), %w(Chinese ch), %w(Japanese jp), %w(Korean kr)]
-    end
     #def update_tutor
       # Destroy tutor object if the user is currently
       # a tutor, and the is_tutor checkbox is unticked.
