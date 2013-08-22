9ba4850f659a5a048fd70d0fb4fae81dbf84a690
Daniele <byakugan.87@gmail.com>
2013-08-22 22:32:58 +0200

    Improved logic to messaging of self and others

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,13 +6,31 @@ class ConversationsController < ApplicationController
   end
   
   def new
-    @conversation = Conversation.new(conversation_params)
-    if @conversation.save
+    sender = current_user.id
+    recipient = params[:recipient_id].to_i
+    convo_by_sender_found = Conversation.find_by_sender_id(sender)
+    convo_by_recipient_found = Conversation.find_by_recipient_id(sender)
+    
+    # Prevent current user from messaging himself.
+    if recipient == sender
+      redirect_to conversations_path
+    
+    # Prevent already messaging participants to create
+    # more than one conversation.
+    elsif convo_by_sender_found
+      @conversation = convo_by_sender_found
       redirect_to @conversation
+    elsif convo_by_recipient_found
+      @conversation = convo_by_recipient_found
+      redirect_to @conversation
+    else
+      @conversation = Conversation.new(conversation_params)
+      redirect_to @conversation if @conversation.save
     end
   end
   
   def show
+    # Prevent seeing/destroying other's messages.
     @conversation = Conversation.find(params[:id])
   end
   
