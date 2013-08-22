84da6a32dc912d054bd16e068eee4e2de9466858
Daniele <byakugan.87@gmail.com>
2013-08-22 23:43:04 +0200

    Refactored new method in Conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,27 +9,28 @@ class ConversationsController < ApplicationController
     sender = current_user.id.to_s
     recipient = params[:recipient_id]
     
-    convo_query =  Conversation.where("(sender_id = ? AND recipient_id = ?) OR (recipient_id = ? AND sender_id = ?)",
-      sender, recipient, sender, recipient).first
-    
     # Prevent current user from messaging himself.
     if recipient == sender
       redirect_to conversations_path
-    # Prevent already messaging participants to create
-    # more than one conversation.
-    elsif convo_query
-      @conversation = convo_query
-      redirect_to @conversation
     else
-      # TODO
-      # Add logic for case where there's no subject, no body.
-      # In that case, don't save.
-      
-      # TODO
-      # Handle case for when an id is missing. In that case,
-      # don't save the message. 
-      @conversation = Conversation.new(conversation_params)
-      redirect_to @conversation if @conversation.save
+      # Prevent already messaging participants to create
+      # more than one conversation.
+      convo_query = Conversation.where("(sender_id = ? AND recipient_id = ?) OR (recipient_id = ? AND sender_id = ?)",
+      sender, recipient, sender, recipient).first
+      if convo_query
+        @conversation = convo_query
+        redirect_to @conversation
+      else
+        # TODO
+        # Add logic for case where there's no subject, no body.
+        # In that case, don't save.
+        
+        # TODO
+        # Handle case for when an id is missing. In that case,
+        # don't save the message.
+        @conversation = Conversation.new(conversation_params)
+        redirect_to @conversation if @conversation.save
+      end
     end
   end
   
