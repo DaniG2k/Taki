1fa6c914b3d93f9690d3c0cb00a4bb7637d07f2a
DaniG2k <byakugan.87@gmail.com>
2013-09-05 00:15:12 +0100

    Added create and catch_not_found methods to Messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  around_action :catch_not_found
+  
   def index
     @messages = Message.all
   end
@@ -6,15 +9,34 @@ class MessagesController < ApplicationController
   def new
     # Message requires a user object
     @tutor = Tutor.find(params[:tutor_id])
-    
-    @message = Message.new
+    if @tutor.id == current_user.id
+      redirect_to tutor_messages_path
+    else
+      @message = Message.new
+    end
+  end
+  
+  def create
+    @tutor = Tutor.find(params[:tutor_id])
+    @message = Message.new(message_params)
     @message.sender = current_user
     @message.recipient = @tutor.user
-    
+
     if @message.save
-      redirect_to tutor_message_path
+      redirect_to tutor_messages_path
     else
       render 'new'
     end
   end
+  
+  private
+    def message_params
+      params.require(:message).permit(:subject, :body)
+    end
+    
+    def catch_not_found
+      yield
+    rescue ActiveRecord::RecordNotFound
+      redirect_to tutor_messages_path, :flash => { :error => "Sorry, record not found." }
+    end
 end
