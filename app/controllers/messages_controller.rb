b5455baf4a9ca353da807de85444aa601331bead
DaniG2k <byakugan.87@gmail.com>
2013-09-04 17:13:09 +0100

    filled in basics for index and new methods for messaging

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,19 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+  
+  def new
+    # Message requires a user object
+    @tutor = Tutor.find(params[:tutor_id])
+    
+    @message = Message.new
+    @message.sender = current_user
+    @message.recipient = @tutor.user
+    
+    if @message.save
+      redirect_to tutor_message_path
+    else
+      render 'new'
+    end
+  end
 end
