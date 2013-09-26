179ea788f44ae736e25dd703b861657fdca73a67
DaniG2k <byakugan.87@gmail.com>
2013-09-26 15:04:53 +0100

    Added parse_proficiency method to TutorsHelper for parsing proficiency integers

--- app/helpers/tutors_helper.rb
+++ app/helpers/tutors_helper.rb
@@ -1,2 +1,14 @@
 module TutorsHelper
+  def parse_proficiency(proficiency_int)
+    case proficiency_int
+    when 1
+      t(:beginner, scope: 'activerecord.attributes.languages.level')
+    when 2
+      t(:intermediate, scope: 'activerecord.attributes.languages.level')
+    when 3
+      t(:advanced, scope: 'activerecord.attributes.languages.level')
+    when 4
+      t(:native, scope: 'activerecord.attributes.languages.level')
+    end
+  end
 end
