76d3d8be4129e034421b70c93668e8e3c70b450a
DaniG2k <byakugan.87@gmail.com>
2013-09-28 19:01:25 +0100

    Prettified cancel account block

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -9,7 +9,7 @@ class Tutor < ActiveRecord::Base
     reject_if: lambda {|attr| %w(university major minor).all? { |val| attr[val].blank? }}
  
   validates_presence_of :user_id
-  validates :rate, presence: true, numericality: true, format: { :with => /\A\d{1,5}(\.\d{0,2})?\z/ }
+  validates :rate, presence: true, numericality: true, format: {:with => /\A\d{1,5}(\.\d{0,2})?\z/}
   validates_length_of :description, maximum: 1000, allow_blank: false
   validate :check_educational_experiences, on: :update
   
--- app/models/tutor_languages.rb
+++ app/models/tutor_languages.rb
@@ -1,3 +1,4 @@
 class TutorLanguages < ActiveRecord::Base
   belongs_to :tutor
+  validates_numericality_of :proficiency, greater_than: 0, less_than: 5, only_integer: true
 end
